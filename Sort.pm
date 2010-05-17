da224bbec4ce947f1fb4f00fcaae3a740c6c8a5e
trapd00r <trapd00r@trapd00r.se>
2010-05-17 07:46:54 +0200

    Media::Sort: huge trouble with the music RE so using ugly hack for now

--- Sort.pm
+++ Sort.pm
@@ -12,7 +12,7 @@ sub getmedia {
 
   my %re = (
     tv    => {
-      regex => '(S[0-9]+)?(E[0-9]+)?(.*TV)|VHS|SWEDISH',
+      regex => '(?i)(S[0-9]+)?(E[0-9]+)?(.*TV)|VHS|SWEDISH',
       location => \@episodes,
     },
     mvids => {
@@ -22,13 +22,24 @@ sub getmedia {
     music => {
       #regex => '(^\w*[^_-])(-|_-_)(\w*[^_-])+-(\(.+\))?',
       #regex => '(^\w*[^_-])(-|_-_)(\w*[^_-])+-(\(.+\))?[^(\w+)',
-      regex  => '.+(:?-|_-_|--|-_-|_)+\w+-?(?:[0-9]{4}?)?(:?[0-9]CD.?)?(:?-\w+)?(:?\(.*\))?-+(:?\(?DAB\)?|\(?WEB\)?|\(?CDA\)?|\(?CDS\)?|\(?CDM\)?|\(?Vinyl\)?|\(?SWE\)?|\(?FI\)?|\(?DE\?|\(?WEB\)?|\(?LINE\)?|\(Promo\)|\(?DVB.?\)?|\(.?LIVE\)?)|\(?Promo_?CD.?\)?',
+      regex => '.+(?:-|_-_)\w+-(?:[0-9]+CD?-)?(?:[0-9]{4}-)?(?:\w+)?',
+      #regex  => '.+(:?-|_-_|--|-_-|_)+\w+-?(?:[0-9]{4}?)?(:?[0-9]CD.?)?(:?-\w+)?(:?\(.*\))?-+(:?\(?DAB\)?|\(?WEB\)?|\(?CDA\)?|\(?CDS\)?|\(?CDM\)?|\(?Vinyl\)?|\(?SWE\)?|\(?FI\)?|\(?DE\?|\(?WEB\)?|\(?LINE\)?|\(Promo\)|\(?DVB.?\)?|\(.?LIVE\)?)|\(?Promo_?CD.?\)?',
       location => \@music,
     },
   );
 
 
   foreach my $file(@files) {
+    if($re{$type} eq 'music') {              #FIXME This so sucks...
+      if($file =~ m/$re{mvids}{regex}/) {
+        push(@mvids, $file);
+      }
+      elsif($file =~ m/$re{music}{regex}/) {
+        push(@music, $file);
+      }
+      last;
+    }
+
     if($file =~ m/$re{$type}{regex}/) {
       push(@{$re{$type}{location}}, $file);
     }
